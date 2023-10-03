fba3b4debcef4d79e605f5383edee8a14d11a9ec
Pasindu Nimsara <pasindu05nimsara@gmail.com>
2023-10-03 19:30:42 +0530

    Android Application (Admin Item View Added)

--- app/src/main/java/com/example/signuploginfirebase/admin_data_view.java
+++ app/src/main/java/com/example/signuploginfirebase/admin_data_view.java
@@ -3,6 +3,7 @@ package com.example.signuploginfirebase;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,6 +24,8 @@ public class admin_data_view extends AppCompatActivity {
     List<DataClass> dataList;
     DatabaseReference databaseReference;
     ValueEventListener eventListener;
+    SearchView searchView;
+    MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,8 @@ public class admin_data_view extends AppCompatActivity {
         setContentView(R.layout.activity_admin_data_view);
 
         recyclerView = findViewById(R.id.recyclerView);
+        searchView = findViewById(R.id.search);
+        searchView.clearFocus();
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(admin_data_view.this, 1);
         recyclerView.setLayoutManager(gridLayoutManager);
@@ -42,7 +47,7 @@ public class admin_data_view extends AppCompatActivity {
 
         dataList = new ArrayList<>();
 
-        MyAdapter adapter = new MyAdapter(admin_data_view.this, dataList);
+        adapter = new MyAdapter(admin_data_view.this, dataList);
         recyclerView.setAdapter(adapter);
 
         databaseReference = FirebaseDatabase.getInstance().getReference("Product List");
@@ -52,8 +57,9 @@ public class admin_data_view extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 dataList.clear();
-                for (DataSnapshot itemSnapshot : snapshot.getChildren()){
+                for (DataSnapshot itemSnapshot: snapshot.getChildren()){
                     DataClass dataClass = itemSnapshot.getValue(DataClass.class);
+                    dataClass.setKey(itemSnapshot.getKey());
                     dataList.add(dataClass);
                 }
                 adapter.notifyDataSetChanged();
@@ -65,5 +71,27 @@ public class admin_data_view extends AppCompatActivity {
                 dialog.dismiss();
             }
         });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                searchList(newText);
+                return true;
+            }
+        });
+    }
+    public void searchList(String text){
+        ArrayList<DataClass> searchList = new ArrayList<>();
+        for (DataClass dataClass: dataList){
+            if (dataClass.getDataTitle().toLowerCase().contains(text.toLowerCase())){
+                searchList.add(dataClass);
+            }
+        }
+        adapter.searchDataList(searchList);
     }
 }
